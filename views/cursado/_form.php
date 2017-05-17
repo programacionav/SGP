1d5ab572535224f2208f9d50af2a9b83dbf4e25b
alamb <alam.ravbar@gmail.com>
2017-05-17 18:28:39 -0300

    Agregar DropList (Select) a form de Cursado

--- views/cursado/_form.php
+++ views/cursado/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use app\models\Materia;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Cursado */
@@ -11,7 +12,16 @@ use yii\widgets\ActiveForm;
 <div class="cursado-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
+    <?php
+      $item = Materia::find() //obtengo un arreglo asociativo[index->titulo]
+        ->select(['Nombre'])  //de noticias donde: lo que esta dentro del option es el titulo
+        ->indexBy('idMateria')       // y el value de los option es el id
+        ->column();
+     ?>
+     <?= $form->field($model, 'idMateria')->dropdownList(
+        $item,
+    ['prompt'=>'Elija la noticia']
+    ); ?>
     <?= $form->field($model, 'fechaInicio')->textInput() ?>
 
     <?= $form->field($model, 'fechaFin')->textInput() ?>
