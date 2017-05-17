e1a823959d8d1d9f3330501623bb2f7ba27d80c8
MarcoSantarelli <marcosavino19@gmail.com>
2017-05-17 18:44:52 -0300

    prueba permisos

--- views/programa/_form.php
+++ views/programa/_form.php
@@ -85,7 +85,7 @@ use yii\widgets\ActiveForm;
     </tbody>
   </table>
   <div class="form-group">
-      <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+      <?= Html::submitButton($model->isNewRecord ? 'crear' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
   </div>
     <?php ActiveForm::end(); ?>
 </div>
