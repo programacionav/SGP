e2c089ca3f5dde0eb5d252311610fcbf16f272e9
hectorara <hector.jara@est.fi.uncoma.edu.ar>
2017-05-29 15:02:09 -0300

    Cambiar nombre de tabla departamentodocentecargo

--- data/29_05_2017_cambio departamento y departamentodocentecargo.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE departamento DROP FOREIGN KEY departamento_ibfk_3;
-ALTER TABLE `departamento` DROP `idDocente`;
-
-RENAME TABLE  `departamentoDocenteCargo` TO  `departamentodocentecargo`
