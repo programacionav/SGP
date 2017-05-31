79cfe060faff23dadf42b809d24e6670c7d51098
nahuel <nahuev086@gmail.com>
2017-05-31 20:37:39 -0300

    Correción de label año

--- models/Programa.php
+++ models/Programa.php
@@ -59,7 +59,7 @@ class Programa extends \yii\db\ActiveRecord
             'idPrograma' => 'Id Programa',
             'idCursado' => 'Id Cursado',
             'orientacion' => 'Orientacion',
-            'anioActual' => 'Anio Actual',
+            'anioActual' => 'Año Actual',
             'programaAnalitico' => 'Programa Analitico',
             'propuestaMetodologica' => 'Propuesta Metodologica',
             'condicionesAcredEvalu' => 'Condiciones Acred Evalu',
@@ -92,11 +92,11 @@ class Programa extends \yii\db\ActiveRecord
         return $this->hasOne(Cursado::className(), ['idCursado' => 'idCursado']);
     }
 
-    //Verifica estado de el programa. 
+    //Verifica estado de el programa.
     public function isabierto()
     {
         if(Cambioestado::find()->where(['idPrograma'=>$this->idPrograma])->count()>0){
-            return 
+            return
             $this::find()
                 ->joinWith('cambioestados')
                 ->where(['cambioestado.idPrograma' => $this->idPrograma,'idEstadoP' => CambioEstado::find()->where(['cambioestado.idPrograma'=>$this->idPrograma])->max('idEstadoP')])
@@ -104,27 +104,27 @@ class Programa extends \yii\db\ActiveRecord
         }
         else{
             return true;
-        }            
+        }
     }
 
     public function afterSave($insert, $changedAttributes) {
         parent::afterSave($insert, $changedAttributes);
-        
+
             $cambioEstado = new Cambioestado;
 
-            $aData['Cambioestado']['idUsuario'] = 1;//Cambiar por usuario logueado 
+            $aData['Cambioestado']['idUsuario'] = 1;//Cambiar por usuario logueado
             $aData['Cambioestado']['fecha'] = date('Y-m-d');
             $aData['Cambioestado']['idEstadoP'] = self::ABIERTO;
             $aData['Cambioestado']['idPrograma'] = $this->idPrograma;
-            
+
             if($cambioEstado->load($aData) && $cambioEstado->save())
             {
                 return true;
             }else{
                 false;
             }
-        
-        
+
+
     }
 
     //Retorna ultimo programa de un cursado especifico
