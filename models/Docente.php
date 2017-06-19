4196d59c9b0653db18fade7834a5196fa08b0bbd
chaarj <jchaarIII@hotmail.com.ar>
2017-06-19 20:41:39 -0300

    Validacion de cuil al crear docente, restriccion de largo de numero en campo cuil de form docente

--- models/Docente.php
+++ models/Docente.php
@@ -55,7 +55,7 @@ class Docente extends \yii\db\ActiveRecord
             [['cuil', 'nombre', 'apellido', 'mail'], 'required'],
 			      [['idDedicacion'], 'required', 'message'=>'Ingrese la dedicacion'],
             [['idDedicacion'], 'integer'],
-            [['cuil'], 'string', 'max' => 20],
+            [['cuil'], 'number'],
             [['nombre', 'apellido'], 'string', 'max' => 50],
             [['mail'], 'string', 'max' => 100],
 			['mail','email'],
